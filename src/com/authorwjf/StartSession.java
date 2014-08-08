64fc36de54c9c27a0d3f8dd104796d096e2d3ced
Bhavneet Singh Ahuja <bhavneet.ahuja@flipkart.com>
2014-08-08 14:16:17 +0530

    fixing app with all three pages

--- src/com/authorwjf/StartSession.java
+++ src/com/authorwjf/StartSession.java
@@ -1,5 +1,46 @@
 package com.authorwjf;
 
-public class StartSession {
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class StartSession extends Activity{
+	Button startSession;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_session);
+        init();
+    }
+	
+	public void init() {
+		startSession = (Button)findViewById(R.id.button3);
+		startSession.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				callMain();
+				
+			}
+		});
+	}
+	
+	public void callMain(){
+		Intent intent=new Intent(this, Main.class);
+		startActivity(intent);
+	}
 
 }
